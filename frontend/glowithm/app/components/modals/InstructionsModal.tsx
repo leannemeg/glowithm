13f02e653231288b70a3ab6060068b094b964fab
leannemeg <main.leannemeg.mason@cvsu.edu.ph>
2025-12-01 17:54:16 +0800

    Added more instructions

--- frontend/glowithm/app/components/modals/InstructionsModal.tsx
+++ frontend/glowithm/app/components/modals/InstructionsModal.tsx
@@ -42,8 +42,9 @@ export default function InstructionsModal({
           <Image source={icons.question_fill} className="self-center w-8 h-8" />
           <Text className="text-inactive font-poppins-regular text-sm text-justify">
             Take a <Text className="font-poppins-bold">clear</Text> photo of
-            your face - a selfie with good lighting and photo quality with no obstructions
-            helps the model to analyze better.
+            your face - a selfie with good lighting and photo quality and your
+            face centered on the frame without obstructions helps the model to
+            analyze better.
           </Text>
           <Text className="text-inactive font-poppins-regular text-sm text-justify">
             For more accurate results, remove{" "}
